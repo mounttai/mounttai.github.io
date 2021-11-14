845cc1f533cc1dc6fb0799b31f82c92d91521e24
Dai Yao <supermounttai@gmail.com>
2021-11-14 17:57:11 +0800

    Update _main.js

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -97,21 +97,19 @@ $(document).ready(function(){
 
 });
 
-
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-    // Get the header
-    var header = document.getElementById("stickyHeader");
+$(document).window.onscroll = function() {
+  // Get the header
+  var header = document.getElementById("stickyHeader");
     
-    // Get the offset position of the navbar
-    var sticky = header.offsetTop;
+  // Get the offset position of the navbar
+  var sticky = header.offsetTop;
+
+  alert(sticky)
     
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
   } else {
     header.classList.remove("sticky");
   }
-}
+};
